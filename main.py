f506c14e6bde2b8ef37b53da667e1df1e11e6178
Zero8One8 <9689388368one@gmail.com>
2026-03-15 01:35:59 +0300

    Update main.py

--- main.py
+++ main.py
@@ -43,7 +43,8 @@ from src.handlers import admin_export
 from src.handlers import admin_scheduler
 from src.handlers import admin_site
 from src.handlers import admin_settings
-from src.handlers.admin_content import router as admin_content_router  # <--- ИСПРАВЛЕНО
+from src.handlers.admin_content import router as admin_content_router
+from src.handlers.admin_stones import router as admin_stones_router
 
 # Настройка логирования
 logging.basicConfig(
@@ -94,7 +95,8 @@ dp.include_router(admin_export.router)
 dp.include_router(admin_scheduler.router)
 dp.include_router(admin_site.router)
 dp.include_router(admin_settings.router)
-dp.include_router(admin_content_router)  # <--- ИСПРАВЛЕНО
+dp.include_router(admin_content_router)
+dp.include_router(admin_stones_router)
 
 # Фоновые задачи
 async def background_tasks():
